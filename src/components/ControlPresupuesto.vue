eecd84a66b605fe57d5a5ef5fd80f2950c2622f7
andres.mera <feli041089@gmail.com>
2023-08-24 14:51:07 -0500

    CSS en el panel de presupuesto

--- src/components/ControlPresupuesto.vue
+++ src/components/ControlPresupuesto.vue
@@ -17,4 +17,65 @@ import imagen from '../assets/img/grafico.jpg';
 </template>
 
 
-<style lang="scss" scoped></style>
+<style scoped>
+.dos-columnas {
+    display: flex;
+    flex-direction: column;
+}
+
+.dos-columnas> :first-child {
+    margin-bottom: 3rem;
+}
+
+@media (min-width: 780px) {
+    .dos-columnas {
+        flex-direction: row;
+        gap: 4rem;
+        align-items: center;
+    }
+
+    .dos-columnas> :first-child {
+        margin-bottom: 0;
+    }
+}
+
+.reset-app {
+    background-color: #DB2777;
+    border: none;
+    padding: 1rem;
+    width: 100%;
+    color: var(--blanco);
+    font-weight: 900;
+    text-transform: uppercase;
+    border-radius: 1rem;
+    transition-property: blackground-color;
+    transition-duration: 300ms;
+}
+
+.reset-app:hover {
+    cursor: pointer;
+    background-color: #c11d67;
+}
+
+.contenedor-presupuesto {
+    width: 100%;
+}
+
+.contenedor-presupuesto p {
+    font-size: 2.4rem;
+    text-align: center;
+    color: var(--gris-oscuro);
+}
+
+@media (min-width: 768px) {
+    .contenedor-presupuesto p {
+        font-size: 2.4rem;
+        text-align: left;
+    }
+}
+
+.contenedor-presupuesto span {
+    font-weight: 900;
+    color: var(--azul);
+}
+</style>
