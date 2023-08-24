b3e16614aae58adb44fb68e28c56efe8e5e877e6
andres.mera <feli041089@gmail.com>
2023-08-24 10:33:36 -0500

    Finalizando el formulario de Presupuesto

--- src/App.vue
+++ src/App.vue
@@ -74,7 +74,7 @@ header h1 {
 .sombra {
     box-shadow: 0px 10px 15px -3px rgba(0, 0, 0, 0.1);
     background-color: var(--blanco);
-    border-radius: 102rem;
+    border-radius: 1.2rem;
     padding: 5rem;
 }
 </style>
--- src/components/Presupuesto.vue
+++ src/components/Presupuesto.vue
@@ -3,13 +3,56 @@
 </script>
 
 <template>
-    <form>
+    <form class="presupuesto">
         <div class="campo">
             <label for="nuevo-presupuesto">Definir Presupuesto</label>
-            <input id="nuevo-presupuesto" class="nuevo-presupuesto" placeholder="Añade tu presupuesto" />
+            <input id="nuevo-presupuesto" class="nuevo-presupuesto" placeholder="Añade tu presupuesto" type="number" />
         </div>
         <input type="submit" value="Definir Presupuesto" />
     </form>
 </template>
 
-<style scoped></style>
+<style scoped>
+.presupuesto {
+    width: 100%;
+}
+
+.campo {
+    display: grid;
+    gap: 2rem;
+}
+
+.presupuesto label {
+    font-size: 2.2rem;
+    text-align: center;
+    color: var(--azul);
+}
+
+.presupuesto input[type="number"] {
+    background-color: var(--gris-claro);
+    border-radius: 1rem;
+    padding: 1rem;
+    border: none;
+    font-size: 2.2rem;
+    text-align: center;
+}
+
+.presupuesto input[type="submit"] {
+    background-color: var(--azul);
+    border: none;
+    border-radius: 1rem;
+    padding: 1rem;
+    text-align: center;
+    font-size: 2rem;
+    margin-top: 2rem;
+    color: var(--blanco);
+    font-weight: 900;
+    width: 100%;
+    transition: background-color 300ms ease;
+}
+
+.presupuesto input[type="submit"]:hover {
+    background-color: #1048A4;
+    cursor: pointer;
+}
+</style>
