6bfad3e741d4072de4b5640eb178f512fba562cd
andres.mera <feli041089@gmail.com>
2023-08-24 10:10:08 -0500

    Primeros pasos con el formulario de presupuesto

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <script setup>
-
+import Presupuesto from './components/Presupuesto.vue';
 </script>
 
 <template>
     <div>
         <header>
             <h1>Planificador de Gastos</h1>
+            <div class="contenedor-header contenedor sombra">
+                <Presupuesto />
+            </div>
         </header>
     </div>
 </template>
@@ -54,5 +57,24 @@ header h1 {
     margin: 0;
     color: var(--blanco);
     text-align: center;
+}
+
+.contenedor {
+    width: 90%;
+    max-width: 80rem;
+    margin: 0 auto;
+}
+
+.contenedor-header {
+    margin-top: -5rem;
+    transform: translateY(5rem);
+    padding: 5rem;
+}
+
+.sombra {
+    box-shadow: 0px 10px 15px -3px rgba(0, 0, 0, 0.1);
+    background-color: var(--blanco);
+    border-radius: 102rem;
+    padding: 5rem;
 }
 </style>
